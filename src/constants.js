57fc69b92a1882e79fc7fc8b900ba4d41caf4aac
kanghongyan <khongyan379@sina.com>
2019-06-03 21:09:02 +0800

    perf: delete useless

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var path = require("path");
-var STATIC_SERVER = 'http://localhost:3000';
-exports.STATIC_SERVER = STATIC_SERVER;
-var LOG_DIR = path.resolve('log');
-exports.LOG_DIR = LOG_DIR;
-var LOG_DIR_DEPLOY = '/opt/nodejslogs';
-exports.LOG_DIR_DEPLOY = LOG_DIR_DEPLOY;
--- src/router/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var decorater_1 = require("./decorater");
-var page = decorater_1.initPageCtrl();
-exports.page = page;
