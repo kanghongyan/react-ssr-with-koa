b33e2bbb33bfd152fcc4fa453e9098ac269f6100
kanghongyan <khongyan379@sina.com>
2019-06-14 20:01:28 +0800

    chore: update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,23 +2,38 @@
 
 import * as Koa from "koa";
 
-declare module ReactSSRWithKoa {
-    function start(app: Koa, option: {useDefaultProxy: boolean, useDefaultSSR: boolean}): Promise<void>;
-
-    function proxyToServer(ctx: Koa.Context, option: object): Promise<any>;
-
-    namespace logger {
-        function init(ins: {info: () => {}, error: () => {}}): void;
-        function info(): void;
-        function error(): void
-    }
-
-    class Html {
-        constructor (ctx: Koa.Context, page: string)
+declare namespace Ns_MainExport {
+    interface Html {
+        new (ctx: Koa.Context, page: string)
         init(option: {ssr?: boolean}): Html
         injectInitialData(data: {dataProps: object, routeProps: object}): Html
         render(customData?: object): Promise<void>
     }
+
+    interface reactSsrWithKoa {
+        start(app: Koa, option: {useDefaultProxy: boolean, useDefaultSSR: boolean}): Promise<void>;
+
+        proxyToServer(ctx: Koa.Context, option: object): Promise<any>;
+
+        logger: {
+            init(ins?: {info: () => {}, error: () => {}}): void;
+            info(msg?): void;
+            error(msg?): void;
+        };
+
+        Html: Html;
+    }
+}
+
+
+declare const MainExport: Ns_MainExport.reactSsrWithKoa;
+
+export = MainExport;
+
+
+
+declare module 'react-ssr-with-koa' {
+    export = MainExport
 }
 
 declare module 'react-ssr-with-koa/WrapperForContainer' {
@@ -36,7 +51,5 @@ declare module 'react-ssr-with-koa/getStoreInitData' {
     export = getStoreInitData
 }
 
-
-export = ReactSSRWithKoa
 
 
